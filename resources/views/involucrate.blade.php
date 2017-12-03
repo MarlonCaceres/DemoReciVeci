58fc79510053dba39e8462aa75e47a98b92f9b7a
Marlon <m_ricky3021@hotmail.com>
2017-12-02 19:32:34 -0500

    ReciVeci V0.2 Formulario

--- resources/views/involucrate.blade.php
+++ resources/views/involucrate.blade.php
@@ -2,6 +2,58 @@
 
 @section('content')
 
-<img src="http://www.fundacionunam.org.mx/wp-content/uploads/2016/03/herpetarioUNAM1.jpg">
+<link rel="stylesheet" type="text/css" href="css/involucrate.css">
+
+<div id="section1" class="fd-tomate blanco centrar seccion1">
+	<div class="row">
+		<div class="col">
+			<h1 class="negrilla">Cómo ser un ReciVeci.</h1>		
+		</div>
+	</div>
+	<br>
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-md-7">
+			<h3>Quieres empezar a ser un ReciVeci en tu barrio.</h3>
+			<h3>Aprende como implementar ReciVeci en tu barrio.</h3>		
+		</div>
+		<div class="col-md-3">
+			<span><a href="files/FAQs.docx" download="Manual ReciVeci" class="descarga">Descargar el<br>Manual ReciVeci</a>	</span>
+		</div>
+		<div class="col-md-1"></div>
+	</div>
+	
+</div>
+
+<div id="section2" class="fd-azul seccion2">
+	<div class="centrar row  blanco">
+		<div class="col">
+			<!--h2 class="negrilla">Voluntarios y Colaboradores</h2-->
+			<h1 class="negrilla">Se parte del Equipo de ReciVeci..!!!</h1>
+		</div>
+	</div>
+	<br>
+	<form action="#" method="get" class="">
+		<div class="form-row " id="formulario_voluntario">	
+			<div class="col-md-1"></div>
+			<div class="col-md-4" class="form-group">
+				<input type="text" name="Nombres" class="form-control" placeholder="Juan José Flores">
+				<input type="text" name="Teléfono" class="form-control" placeholder="0221234567">
+				<input type="text" name="Teléfono" class="form-control" placeholder="0221234567">
+			</div>
+			<div class="col-md-1"></div>
+			<div class="col-md-4">
+				<input type="text" name="area" class="form-control" placeholder="Aréa">
+				<input type="text" name="Correo" class="form-control" placeholder="JuanJose@reciveci.com">
+				<input type="text" name="Teléfono" class="form-control" placeholder="0221234567">
+			</div>
+			<div class="col-md-2 centrar">
+
+				<button class="vertical btn-enviar negrilla blanco" >ENVIAR</button>
+			</div>
+		</div>
+	</form>
+</div>
+
 
 @stop
